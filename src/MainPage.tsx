59d7222788ab5814eb6c9314b99b77795eed0122
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-29 15:58:49 +0100

    refactor: use lint

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
 const Main: React.FC = () => (
   <div>
     <h2>Main Page</h2>
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/uncontrolled-form">Uncontrolled Form</Link></li>
-        <li><Link to="/hook-form">React Hook Form</Link></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/uncontrolled-form">Uncontrolled Form</Link>
+        </li>
+        <li>
+          <Link to="/hook-form">React Hook Form</Link>
+        </li>
       </ul>
     </nav>
   </div>
