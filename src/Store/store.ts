ea07819c7e9cb292054bb965a1573af3dc351501
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-29 17:00:22 +0100

    feat: add store and form Slice

---
+++ src/Store/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import formReduser from "./formReduser";
+
+const store = configureStore({
+  reducer: {
+    form: formReduser,
+  },
+});
+
+export default store;
