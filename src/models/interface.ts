dae9407747dae020aa10ca39c67928ce2648f4bd
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-12-01 18:25:56 +0100

    feat: add to validate schema rules for image and country

---
+++ src/models/interface.ts
@@ -0,0 +1,4 @@
+export interface FileObject {
+  size: number;
+  type: string;
+}
--- src/validation/validSchema.tsx
+++ src/validation/validSchema.tsx
@@ -1,18 +1,27 @@
 import * as yup from 'yup';
+import { FileObject } from '../models/interface'
 
 export const validationSchema = yup.object().shape({
   name: yup
     .string()
     .required('Name is required')
-    .matches(/^[A-Z][a-z]*$/, 'Name should start with an uppercase letter'),
+    .matches(
+      /^([A-Z][a-z]*)(-[A-Z][a-z]*)*([ ][A-Z][a-z]*)*$/,
+      'Name should consist of one or more words, each starting with an uppercase letter'
+    ),
   age: yup
     .number()
     .required('Age is required')
-    .min(0, 'Age should be a positive number'),
+    .positive('Age should be a positive number')
+    .integer('Age should be an integer'),
   email: yup
     .string()
     .required('Email is required')
     .email('Invalid email format'),
+  confirmEmail: yup
+    .string()
+    .required('Email confirmation is required')
+    .oneOf([yup.ref('email')], 'Emails must match'),
   password: yup
     .string()
     .required('Password is required')
@@ -24,7 +33,30 @@ export const validationSchema = yup.object().shape({
     .string()
     .required('Password confirmation is required')
     .oneOf([yup.ref('password')], 'Passwords must match'),
-  gender: yup.string().required('Select a gender'),
+  gender: yup
+    .string()
+    .required('Select a gender')
+    .oneOf(['male', 'female', 'other'], 'Invalid gender'),
   acceptTerms: yup.boolean().oneOf([true], 'You must accept the terms'),
-  // Add validation rules for image and country
+  image: yup
+  .mixed()
+  .test(
+    'fileSize',
+    'File size is too large',
+    (value) => {
+      const file: FileObject | undefined = value as FileObject | undefined;
+      if (!file) return true;
+
+      const fileSize = file.size;
+      const fileType = file.type;
+
+      const isSizeValid = fileSize <= 1024 * 1024; // 1 MB
+      const isTypeValid = ['image/png', 'image/jpeg'].includes(fileType);
+
+      return isSizeValid && isTypeValid;
+    }
+  ),
+  countryId: yup
+    .string()
+    .required('Select a country')
 });
